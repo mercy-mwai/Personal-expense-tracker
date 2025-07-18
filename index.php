67550928e7283705dbc2d245644e5b44b448f086
mercy-mwai <mercyneemam@gmail.com>
2025-07-18 11:42:13 +0300

    updates

---
+++ index.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        form {margin:10px;
+             padding-top:10px}
+
+         input, select, button {
+            margin-bottom: 10px;
+            padding: 8px;
+            width: 100%;
+            max-width: 300px;
+            display: block;
+        }
+    </style>
+</head>
+<body>
+    <form action="index.php" method="POST">
+    <input type="text" name="amount" placeholder="Amount" required />
+    <br />
+    <select name="category_id" required>
+`       <option value="1">Rent</option>
+`       <option value="2">Transport</option>
+`       <option value="3">Food</option>
+    </select>
+    <br />
+    <input type="text" name="description" placeholder="Description" />
+    <br />
+    <input type="date" name="date" required />
+    <br />
+    <button type="submit" name="add-expense">Add Expense</button>
+    </form>
+</body>
+</html>
