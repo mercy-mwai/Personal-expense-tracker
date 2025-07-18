4459b554e81530060b09513966c5f74df53c4a2c
mercy-mwai <mercyneemam@gmail.com>
2025-07-18 12:10:27 +0300

    updates

--- Expensescontroller.php
+++ Expensescontroller.php
@@ -1,8 +1,12 @@
 <?php
+
+if(isset($_POST["add-expense"])){
 $amount = $_POST['amount'];
 $categoryId= $_POST['category-id'];
 $date= $_POST['date'];
 $description =$_POST['description'];
+
+}
 
 
 ?>
