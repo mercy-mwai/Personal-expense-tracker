3204c0257e8bc6ea859899474ffbec1fcbb33655
mercy-mwai <mercyneemam@gmail.com>
2025-07-18 12:41:01 +0300

    updates

--- Expensescontroller.php
+++ Expensescontroller.php
@@ -2,14 +2,18 @@
 
 if(isset($_POST["add-expense"])){
 
-$amount = filter_input(INPUT-POST, "amount",FILTER-VALIDATE-FLOAT );
+$amount = filter_input(INPUT_POST, "amount",FILTER-VALIDATE-FLOAT );
 
-$categoryId=filter_input(INPUT-POST, "category-id", FILTER-VALIDATE-INT);
-$date= filter_input(INPUT-POST, "date", FILTER-SANITIZE-STRING);
-$description =filter_input(INPUT-POST, "description", FILTER_SANITIZE_FULL-SPECIAL_CHARS);
+$categoryId=filter_input(INPUT_POST, "category-id", FILTER-VALIDATE-INT);
+$date= filter_input(INPUT_POST, "date", FILTER-SANITIZE-STRING);
+$description =filter_input(INPUT_POST, "description", FILTER_SANITIZE_FULL-SPECIAL_CHARS);
 
+if($amount & $categoryId & $date){
+    echo "Valid Input";
+}else{
+    echo "Invalid Input . Please check your data";
+}
 
-echo $amount;
 }
 
 
