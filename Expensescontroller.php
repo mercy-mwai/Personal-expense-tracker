cd4670a89dce875249ee2eca144e1a4816ad9aa3
mercy-mwai <mercyneemam@gmail.com>
2025-07-20 10:13:50 +0300

    updates

--- Expensescontroller.php
+++ Expensescontroller.php
@@ -1,5 +1,5 @@
 <?php
-
+include "database.php";
 if(isset($_POST["add-expense"])){
 
 $amount = filter_input(INPUT_POST, "amount",FILTER_VALIDATE_FLOAT );
@@ -7,6 +7,7 @@ $amount = filter_input(INPUT_POST, "amount",FILTER_VALIDATE_FLOAT );
 $categoryId=filter_input(INPUT_POST, "category-id", FILTER_VALIDATE_INT);
 $date= filter_input(INPUT_POST, "date", FILTER_SANITIZE_STRING);
 $description =filter_input(INPUT_POST, "description", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
 
 if($amount !== false && $categoryId !==false && !empty($date)){
     echo "Valid Input";
--- database.php
+++ database.php
@@ -1,3 +1,22 @@
 <?php
+$db_server ="localhost";
+$db_user= "root";
+$db_password= "";
+$db_name ="expensesdb";
+$conn ="";
 
+
+try{
+$conn=new mysqli($db_server,
+            $db_user,
+            $db_password,
+            $db_name);
+
+}catch(mysqli_sql_exception){
+        echo "You are not connected";
+}
+
+if($conn){
+    echo "Your connection is valid";
+}
 ?>
