61ee9b08878965b3c1359682a424ee6fb85e5173
mercy-mwai <mercyneemam@gmail.com>
2025-07-20 09:04:52 +0300

    updates

--- Expensescontroller.php
+++ Expensescontroller.php
@@ -8,7 +8,7 @@ $categoryId=filter_input(INPUT_POST, "category-id", FILTER_VALIDATE_INT);
 $date= filter_input(INPUT_POST, "date", FILTER_SANITIZE_STRING);
 $description =filter_input(INPUT_POST, "description", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-if($amount ===false || $categoryId===false || empty($date)){
+if($amount !== false && $categoryId !==false && !empty($date)){
     echo "Valid Input";
 }else{
     echo "Invalid Input . Please check your data";
