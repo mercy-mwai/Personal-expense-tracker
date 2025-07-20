1697e9f919a3dbe9aa00ad3856b5e307fa92ed1b
mercy-mwai <mercyneemam@gmail.com>
2025-07-20 16:39:03 +0300

    updates

--- Expensescontroller.php
+++ Expensescontroller.php
@@ -4,7 +4,7 @@ if(isset($_POST["add-expense"])){
 
 $amount = filter_input(INPUT_POST, "amount",FILTER_VALIDATE_FLOAT );
 
-$categoryId=filter_input(INPUT_POST, "category-id", FILTER_VALIDATE_INT);
+$categoryId=filter_input(INPUT_POST, "category_id", FILTER_VALIDATE_INT);
 $date= filter_input(INPUT_POST, "date", FILTER_SANITIZE_STRING);
 $description =filter_input(INPUT_POST, "description", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
