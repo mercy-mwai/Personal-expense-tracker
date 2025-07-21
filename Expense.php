f1bdd80696faad5b96ef4af7ff953c9ba03c77f1
mercy-mwai <mercyneemam@gmail.com>
2025-07-21 10:34:41 +0300

    updates

--- Expense.php
+++ Expense.php
@@ -22,5 +22,25 @@ class Expense{
     }
 }
 
+public function getAll(){
+    global $conn;
+
+    $sql= "SELECT * FROM expenses JOIN categories ON expenses.category_id =categories.id" ;
+
+    $result =$conn->query($sql);
+
+    if($result && $result->num_rows >0){
+        $expenses =[];
+
+         while ($row = $result->fetch_assoc()) {
+         $expenses[] = $row;
+        }
+
+        return $expenses; 
+    }else{
+        return [];
+    }
+}
+
 }
 ?>
