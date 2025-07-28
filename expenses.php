d3fd7528f1b41511105c7e9bc3e19617ba3b24a0
mercy-mwai <mercyneemam@gmail.com>
2025-07-28 18:37:04 +0300

    updates

--- expenses.php
+++ expenses.php
@@ -6,7 +6,7 @@ $expense = new Expense();
 $allExpenses = $expense->getAll();
 
 foreach ($allExpenses as $item) {
-    echo $item['amount'] . " - " . $item['category_id'] . $item["date"] . $item["description"]."<br>";
+    echo $item ['amount'] . " - " . $item['category_id'] . $item["date"] . $item["description"]."<br>";
 }
 
 
