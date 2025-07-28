291ea74d0bb03873270c8b3027a3c25353056060
mercy-mwai <mercyneemam@gmail.com>
2025-07-28 19:06:47 +0300

    updates

--- Allexpenses.php
+++ Allexpenses.php
@@ -5,14 +5,6 @@ require_once "ExpensesController.php";
 $expense = new Expense();
 $allExpenses = $expense->getAll();
 
-// foreach ($allExpenses as $item) {
-//     echo "Amount" . $item['amount'] . "<br>";
-//     echo "Category" . $item['category_id'] . "<br>";
-//     echo "Date" . $item['date'] . "<br>";
-//     echo "Description" . $item ['description'] ."<br>";
-//     echo "<hr>";
-// }
-
 echo "<table border='1' cellpadding='10'>";
 echo "<tr>
         <th>Amount</th>
@@ -24,7 +16,7 @@ echo "<tr>
 foreach ($allExpenses as $item) {
     echo "<tr>";
     echo "<td>" . $item['amount'] . "</td>";
-    echo "<td>" . $item['category_id'] . "</td>"; // Use $item['category_name'] if you joined
+    echo "<td>" . $item['category_id'] . "</td>"; 
     echo "<td>" . $item['date'] . "</td>";
     echo "<td>" . $item['description'] . "</td>";
     echo "</tr>";
