2fb2c2681bbf53ea4f567ec94967708da0df1ca8
mercy-mwai <mercyneemam@gmail.com>
2025-07-31 12:37:54 +0300

    updates

--- delete.php
+++ delete.php
@@ -1,3 +1,19 @@
 <?php
+require_once "Expense.php";
 
+if(isset($_GET['id'])){
+$id =(int) $_GET['id'];
+
+$expense =new Expense();
+$result =$expense->delete($id);
+
+if($result === true){
+    header("Location : index.blade.php?deleted=1");
+    exist;
+}else{
+    echo "Error deleting expense: $result";
+}
+}else{
+    echo "No ID provided";
+}
 ?>
